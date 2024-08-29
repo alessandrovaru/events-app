47bfd22244dda809431e2d02d7d5b948d310ab58
Alessandro <alessandrovaru@gmail.com>
2024-08-29 16:31:41 -0400

    BUTTONS ON SESSION

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -12,11 +12,12 @@ export const Header = async ({tokens}) => {
         <Link className="py-2 px-4 rounded-lg text-sm font-medium hover:underline underline-offset-4" href="#">
           Clases
         </Link>
-        <Link className="py-2 px-4 rounded-lg bg-white text-black hover:bg-gray-200 text-sm font-medium" href="/signin">
-          Iniciar sesión
-        </Link>
-        {tokens && (
+        {tokens ? (
           <Logout email={tokens?.decodedToken.email}/>
+        ): (
+          <Link className="py-2 px-4 rounded-lg bg-white text-black hover:bg-gray-200 text-sm font-medium" href="/login">
+            Iniciar sesión
+          </Link>
         )}
       </nav>
     </header>
