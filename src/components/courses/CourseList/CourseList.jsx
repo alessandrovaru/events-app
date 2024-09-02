349658429784547c5170b2f72c6b48fc80919f6c
Alessandro <alessandrovaru@gmail.com>
2024-09-02 14:18:55 -0400

    Adding locked cards

--- src/components/courses/CourseList/CourseList.jsx
+++ src/components/courses/CourseList/CourseList.jsx
@@ -1,5 +1,6 @@
 import { getFirestore } from "firebase-admin/firestore";
 import { getFirebaseAdminApp } from '@/app/firebase';
+import { LockKeyhole } from "lucide-react";
 
 const db = getFirestore(getFirebaseAdminApp());
 
@@ -32,12 +33,18 @@ export const CourseList = async ({ tokens }) => {
       <h2 className="text-3xl font-bold text-white mb-6 ps-6 pt-6">Cursos</h2>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4 p-6">
         {sortedCourses.map(course => (
-          <div key={course.id} className="course-card p-6 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+          <div key={course.id} className="relative course-card p-6 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
             <h2 className="text-2xl font-bold mb-2 text-gray-700">{course.name}</h2>
             <p className="text-gray-700 mb-4">{course.description}</p>
             <p className="text-gray-900"><strong>Discipline:</strong> {course.discipline}</p>
-            {course.id === userCourseId && (
-              <p className="text-green-500 font-bold">Inscrito</p>
+            {course.id !== userCourseId ? (
+             <>
+              <div className="absolute bg-black h-full w-full top-0 left-0 opacity-50 hover:opacity-80 transition-opacity duration-300 flex items-center justify-center rounded-lg cursor-pointer">
+                <LockKeyhole />
+              </div>
+             </>
+            ): (
+              <></>
             )}
           </div>
         ))}
