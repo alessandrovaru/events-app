4d3fba74f9fce1df22c8085e14a93ad200378f51
Alessandro <alessandrovaru@gmail.com>
2024-09-02 13:08:09 -0400

    Adjusting colors inbheader buttons

--- src/app/Logout.jsx
+++ src/app/Logout.jsx
@@ -24,13 +24,13 @@ export default function Logout({ email }) {
     <>
       <Link
         href="/profile"
-        className="text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-primary-800"
+        className="text-white bg-red-900 bg-red-950 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-900 dark:hover:bg-red-950 dark:focus:ring-primary-800"
       >
         {user.displayName}
       </Link>
       <button
         onClick={handleLogout}
-        className="text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-primary-800"
+        className="text-white bg-red-900 bg-red-950 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-900 dark:hover:bg-red-950 dark:focus:ring-primary-800"
       >
         Logout
       </button>
