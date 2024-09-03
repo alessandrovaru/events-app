6f7f4eaf98146f3c694792ec6f8b27f619a68a7a
Alessandro <alessandrovaru@gmail.com>
2024-09-03 13:59:11 -0400

    Eliminating admin

--- src/app/admin/layout.js
+++
@@ -1,9 +0,0 @@
-import { AuthContextProvider } from "@/context/AuthContext";
-
-export default function AdminLayout({ children }) {
-  return (
-    <AuthContextProvider>
-      {children}
-    </AuthContextProvider>
-  );
-}
