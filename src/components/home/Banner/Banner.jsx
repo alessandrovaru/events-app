231ce64a0c46fc725d80b8aa12db0a00a427a326
Alessandro <alessandrovaru@gmail.com>
2024-09-05 13:33:09 -0400

    Banner working with data

--- src/components/home/Banner/Banner.jsx
+++ src/components/home/Banner/Banner.jsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 
+const bannerData =
+  {
+    image: "/images/banner.jpg",
+    alt: "banner"
+  };
+
 export function Banner() {
   return (
     <section className="relative w-full py-12 md:py-24 lg:py-32  flex items-center justify-center bg-black">
-      <Image src={'/images/banner.jpg'} alt={`banner`} className="h-full w-full mb-2 object-cover z-0" fill />
+      <Image src={bannerData.image} alt={bannerData.alt} className="h-full w-full mb-2 object-cover z-0" fill />
     </section>
   );
 }
