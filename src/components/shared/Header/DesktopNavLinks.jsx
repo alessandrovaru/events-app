715133d035c2a0a4f81560805070ca27d6771dc7
Alessandro <alessandrovaru@gmail.com>
2024-09-12 11:59:18 -0400

    Adding admin validation

--- src/components/shared/Header/DesktopNavLinks.jsx
+++ src/components/shared/Header/DesktopNavLinks.jsx
@@ -1,6 +1,22 @@
 import Logout from '@/app/Logout';
+import listData from '@/firebase/firestore/listData';
 import Link from 'next/link';
-export default function DesktopNavLinks({email}) {
+import { useEffect, useState } from 'react';
+
+export default function DesktopNavLinks({ email, uid }) {
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    async function fetchDbUser() {
+      const users = await listData('users');
+      const currentUser = users.find(user => user.id === uid);
+      if (currentUser && currentUser.role === 'admin') {
+        setIsAdmin(true);
+      }
+    }
+    fetchDbUser();
+  }, [uid]);
+
   return (
     <ul className="absolute right-0 flex flex-row space-x-6">
       <li className="text-xl hover:text-blue-900 hover:font-semibold flex items-center">
@@ -8,8 +24,15 @@ export default function DesktopNavLinks({email}) {
           Dashboard
         </Link>
       </li>
+      {isAdmin && (
+        <li className="text-xl hover:text-blue-900 hover:font-semibold flex items-center">
+          <Link className="py-2 px-4 rounded-lg text-sm font-medium hover:underline underline-offset-4" href="/admin">
+            Admin
+          </Link>
+        </li>
+      )}
       <li className="text-xl hover:text-blue-900 hover:font-semibold">
-        <Logout email={email}/>
+        <Logout email={email} />
       </li>
       {/* Add other navigation links */}
     </ul>
--- src/components/shared/Header/HeaderLogged.jsx
+++ src/components/shared/Header/HeaderLogged.jsx
@@ -17,24 +17,10 @@ export const HeaderLogged = ({tokens}) => {
   }
 
   return (
-    // <header className="px-4 lg:px-6 flex items-center h-[10dvh]">
-    //   <Link className="flex items-center justify-center" href="#">
-    //     <Image src="/images/logo.jpg" alt="Total Elite Training" width={40} height={40} />
-    //     <span className="font-bold">Total Elite Training</span>
-    //   </Link>
-    //   <nav className="ml-auto flex gap-4 sm:gap-6">
-    //     <Link className="py-2 px-4 rounded-lg text-sm font-medium hover:underline underline-offset-4" href="/dashboard">
-    //       Dashboard
-    //     </Link>
-    //     <Logout email={tokens?.decodedToken.email}/>
-    //   </nav>
-      
-    // </header>
-
     <>
       <div className="hidden relative py-6 sm:flex flex-col justify-center">
         <LogoLink />
-        <DesktopNavLinks email={tokens?.decodedToken.email} />
+        <DesktopNavLinks email={tokens?.decodedToken.email} uid={tokens?.decodedToken.uid} />
       </div>
       <div className="sm:hidden relative flex flex-row my-4">
         <LogoLink />
