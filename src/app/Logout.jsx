1e548e3dcd74c778097c806e03d20f712e16db61
Alessandro <alessandrovaru@gmail.com>
2024-09-15 18:28:33 -0400

    adding buttons to profile

--- src/app/Logout.jsx
+++ src/app/Logout.jsx
@@ -34,9 +34,9 @@ export default function Logout({ email, noProfile }) {
       )}
       <button
         onClick={handleLogout}
-        className="text-white bg-red-900 bg-red-950 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-900 dark:hover:bg-red-950 dark:focus:ring-primary-800 mr-4"
+        className={`bg-red-500 text-white p-2 rounded-lg hover:bg-red-700 transition ${!noProfile ? 'mr-4' : ''}`}
       >
-        Logout
+        Cerrar sesión
       </button>
     </>
   );
