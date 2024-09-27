4890742449bc4065bbcaebc46eb41c1daf21f417
Alessandro <alessandrovaru@gmail.com>
2024-09-27 12:55:36 -0400

    Registering service worker

--- src/components/shared/Header/HeaderLogged.jsx
+++ src/components/shared/Header/HeaderLogged.jsx
@@ -31,6 +31,19 @@ export const HeaderLogged = ({tokens}) => {
     }
   }, []);
 
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/firebase-messaging-sw.js")
+        .then((registration) => {
+          console.log("Service Worker registrado con éxito:", registration);
+        })
+        .catch((error) => {
+          console.error("Error al registrar el Service Worker:", error);
+        });
+    }
+  }, []);
+
   return (
     <>
     <NotificationHandler />
