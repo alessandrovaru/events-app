5ccc32083589971bc7303c05dff92a7f5aa77546
Alessandro <alessandrovaru@gmail.com>
2024-09-27 13:07:23 -0400

    Client config

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,33 +1,37 @@
-// public/firebase-messaging-sw.js
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
 
+
+// are not available in the service worker.
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-// Realiza una solicitud a tu API Route para obtener la configuración de Firebase
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    fetch('/api/firebaseConfig')
-      .then((response) => response.json())
-      .then((config) => {
-        firebase.initializeApp(config);
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+  apiKey: "AIzaSyCMQ3f3baXs0ZV0p_p95V4m6xQUpASk9aA",
+  authDomain: "total-elite-training.firebaseapp.com",
+  projectId: "total-elite-training",
+  storageBucket: "total-elite-training.appspot.com",
+  messagingSenderId: "355745487804",
+  appId: "1:355745487804:web:9ab80e039ee3fd9e4ba9b9",
+  measurementId: "G-PP4SYLBVBG"
+});
 
-        const messaging = firebase.messaging();
+const messaging = firebase.messaging();
 
-        messaging.onBackgroundMessage((payload) => {
-          console.log('[firebase-messaging-sw.js] Received background message ', payload);
-
-          // Personaliza la notificación aquí
-          const notificationTitle = payload.notification?.title || 'Sin título';
-          const notificationOptions = {
-            body: payload.notification?.body || 'Sin contenido',
-            icon: payload.notification?.image || '/next.svg',  // Ruta a tu icono por defecto si no está definido
-          };
-
-          self.registration.showNotification(notificationTitle, notificationOptions);
-        });
-      })
-      .catch((error) => {
-        console.error('Error al obtener la configuración de Firebase:', error);
-      })
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    '[firebase-messaging-sw.js] Received background message ',
+    payload
   );
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.image,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
